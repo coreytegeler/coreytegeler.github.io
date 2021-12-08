73f367c4adba237395ed9b0ad19410516e4be082
Corey Tegeler <coreytegeler@gmail.com>
2021-12-07 19:23:09 -0500

    update .gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c8718b4026d370ab"; }
+/******/ 		__webpack_require__.h = function() { return "993250cb43b13a6d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
