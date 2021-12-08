7f67a3458188b954a1f56935deff5f30903eacbe
Corey Tegeler <coreytegeler@gmail.com>
2021-12-07 23:46:33 -0500

    add repo path

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 const withSass = require('@zeit/next-sass')
 module.exports = {
   reactStrictMode: true,
-  cssModules: true
+  cssModules: true,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+	assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
