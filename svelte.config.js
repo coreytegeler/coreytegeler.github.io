c92243400899693dc8f9851694c027fc21081659
Corey Tegeler <coreytegeler@gmail.com>
2025-05-08 23:01:43 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
 import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
+import { sveltePreprocess } from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
+	preprocess: sveltePreprocess(),
 	kit: {
 		alias: {
 			$src: './src',
@@ -16,7 +16,8 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: '404.html',
+			fallback: 'index.html',
+			// fallback: null,
 			precompress: false,
 			strict: true
 		}),
